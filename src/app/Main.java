bac6cf60f710ddc607a43dfdfb8d16bc351d454a
AleehSophia <alessia.casado@gmail.com>
2022-09-10 19:10:38 -0300

    :sparkles:feat: Main class implemented

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,46 @@
 package app;
 
+import model.entities.Account;
+import model.exceptions.AccountException;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Account data");
+
+        System.out.print("Number: ");
+        int n = sc.nextInt();
+
+        System.out.print("Holder: ");
+        sc.nextLine();
+        String name = sc.nextLine();
+
+        System.out.print("Initial balance: ");
+        Double balance = sc.nextDouble();
+
+        System.out.print("Withdraw limit: ");
+        Double withdrawLimit = sc.nextDouble();
+
+        Account acc = new Account(n, name, balance, withdrawLimit);
+
+        System.out.println();
+        System.out.print("Enter a withdraw amount: ");
+        Double withdrawAmount = sc.nextDouble();
+
+        try {
+            acc.withdraw(withdrawAmount);
+            System.out.println("New balance: " + String.format("%.2f", acc.getBalance()));
+        }
+        catch (AccountException e) {
+            System.out.println(e.getMessage());
+        }
+
+        sc.close();
     }
 }
